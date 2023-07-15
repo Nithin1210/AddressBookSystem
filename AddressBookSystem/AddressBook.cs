977c5098fcd5dfe38bdef9dbaec9ccb187269bbc
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-07-15 20:59:30 +0530

    [Nithin] Add :updation of Contact_creation

---
+++ AddressBookSystem/AddressBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class AddressBook
+    {
+        public void CreateContact()
+        {
+            Console.WriteLine("Enter the details :\n1.FirstName \n2.LastName\n3.Address\n4.City\n5.State" +
+                "\n6.Zip\n7.PhoneNumber\n8.Email ");
+            Contact contact = new Contact()
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToInt32(Console.ReadLine()),
+                PhoneNumber = Convert.ToInt32(Console.ReadLine()),
+                Email = Console.ReadLine()
+
+            };
+            Console.WriteLine(contact.FirstName + "\n " + contact.LastName + "\n " + contact.Address + "\n " + contact.City +
+                "\n " + contact.State + "\n " + contact.Zip + "\n " + contact.PhoneNumber + "\n " + contact.Email);
+
+
+        }
+    }
+}
---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,13 @@
+﻿using AddressBookSystem;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("welcome to address book :-:-");
+        AddressBook book = new AddressBook();
+        book.CreateContact();
+
+    }
+}
