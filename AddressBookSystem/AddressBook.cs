541d3527ed497148115ed853a57a58925434500f
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-07-17 10:25:11 +0530

    [Nithin] Add: updation of uc2

--- AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBook.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace AddressBookSystem
 {
     internal class AddressBook
     {
+        List<Contact> addressBook = new List<Contact>();
         public void CreateContact()
         {
             Console.WriteLine("Enter the details :\n1.FirstName \n2.LastName\n3.Address\n4.City\n5.State" +
@@ -26,6 +28,7 @@ namespace AddressBookSystem
             };
             Console.WriteLine(contact.FirstName + "\n " + contact.LastName + "\n " + contact.Address + "\n " + contact.City +
                 "\n " + contact.State + "\n " + contact.Zip + "\n " + contact.PhoneNumber + "\n " + contact.Email);
+            addressBook.Add(contact);
 
 
         }
