24c3de5be600ddb42647ef26f8a1ad8bf503ddc9
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-07-17 11:08:51 +0530

    [Nithin] Add: updation of Edit Contact

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -3,11 +3,29 @@ using System;
 
 internal class Program
 {
-    private static void Main(string[] args)
+     static void Main(string[] args)
     {
-        Console.WriteLine("welcome to address book :-:-");
-        AddressBook book = new AddressBook();
-        book.CreateContact();
+        Console.WriteLine("Enter the option to be proceded : \n1.Create Contact \n2.Edit Contact \n3.Exit");
+        bool flag = true;
+        AddressBook addressbook = new AddressBook();
+        while(flag)
+        {
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    addressbook.CreateContact();
+                    break;
+                case 2:
+                    Console.WriteLine("Enter name To edit Contact Details :-");
+                    string input = Console.ReadLine();
+                    break;
+                case 3:
+                    flag = false;
+                    break;
+
+            }
+        }
 
     }
 }
